8d351c55cf9700f1a0c2a8a488d0d7ef197b7af1
Navin-Gadgi <gadginavinp@gmail.com>
2026-01-13 15:33:24 +0530

    Make navbar responsive

--- libmasy/settings.py
+++ libmasy/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-t1!fyv3hbwmg!i_gckh9((uoq+^s*0t0ot9mghku%we+y)1mg8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '.onrender.com'
+    ]
 
 
 # Application definition
