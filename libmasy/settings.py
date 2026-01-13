867b83588ca2fee8ddcb99ccfd824148b938f083
Navin-Gadgi <gadginavinp@gmail.com>
2026-01-14 01:34:41 +0530

    Change database to postgrespl

--- libmasy/settings.py
+++ libmasy/settings.py
@@ -80,12 +80,43 @@ WSGI_APPLICATION = 'libmasy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'YZZjqpiClLOYgFKUjQHGaDuLeQzebwJG',
+#         'HOST': 'postgres.railway.internal',
+#         'PORT': '5432',
+#     }
+# }
+
+import dj_database_url
+
+# DATABASE_URL = 'postgresql://postgres:YZZjqpiClLOYgFKUjQHGaDuLeQzebwJG@ballast.proxy.rlwy.net:29397/railway'
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL = 'postgresql://postgres:YZZjqpiClLOYgFKUjQHGaDuLeQzebwJG@ballast.proxy.rlwy.net:29397/railway',
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
